7d0ec973f2e66afeb0fe22acd99fe9ecc9d89471
Simon Johansson <simon@editd.com>
2012-07-02 22:13:58 +0100

    First commit to a cool text based game

---
+++ src/text_adventure/core.clj
@@ -0,0 +1,17 @@
+(ns text-adventure.core)
+
+(def objects '(whiskey-bottle bucket frog chain))
+
+(def game-map (hash-map
+               'living-room '((you are in the living room
+                                   of a wizards house - there is a wizard
+                                   snoring loudly on the couch -)
+                              (west door garden)
+                              (upstairs stairway attic))
+               'garden '((you are in a beautiful garden -
+                              there is a well in front of you -)
+                         (east door living-room))
+               'attic '((you are in the attic of the
+                             wizards house - there is a giant
+                             welding torch in the corner -)
+                        (downstairs stairway living-room))))
