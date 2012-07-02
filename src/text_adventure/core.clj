d1a330d24fea3698428200322b8a6bcd74abea62
Simon Johansson <simon@editd.com>
2012-07-03 00:23:43 +0100

    Add some actions, game is now complete

--- src/text_adventure/core.clj
+++ src/text_adventure/core.clj
@@ -24,6 +24,10 @@
 
 (def location 'living-room)
 
+(def chain-welded? false)
+
+(def bucket-filled? false)
+
 (defn describe-location [location game-map]
   (first (location game-map)))
 
@@ -79,3 +83,36 @@
 
 (defspel pickup [object]
   `(spel-print (pickup-object '~object)))
+
+(defspel game-action [command subj obj place & args]
+  `(defspel ~command [subject# object#]
+     `(spel-print (cond (and (= location '~'~place)
+                             (= '~subject# '~'~subj)
+                             (= '~object# '~'~obj)
+                             (have? '~'~subj))
+                        ~@'~args
+                        :else '(i cannot ~'~command like that -)))))
+
+(game-action weld chain bucket attic
+             (cond (and (have? 'bucket) (def chain-welded true))
+                   '(the chain is now securely welded to the bucket -)
+                   :else '(you do not have a bucket -)))
+
+(game-action dunk bucket well garden
+             (cond chain-welded
+                   (do (def bucket-filled true)
+                       '(the bucket is now full of water))
+                   :else '(the water level is too low to reach -)))
+
+(game-action splash bucket wizard living-room
+             (cond (not bucket-filled) '(the bucket has nothing in it -)
+                   (have? 'frog) '(the wizard awakens and sees that you stole
+                                       his frog -
+                                       he is so upset he banishes you to the
+                                       netherworlds - you lose! the end -)
+                   :else '(the wizard awakens from his slumber and greets you
+                               warmly -
+                               he hands you the magic low-carb donut - you win!
+                               the end -)))
+
+(look)
